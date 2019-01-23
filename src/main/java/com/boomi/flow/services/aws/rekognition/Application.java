7c7d21b10b3e80f722a805ae63388463a9afa33c
Jose Collazzi <josecollazzi@gmail.com>
2019-01-23 12:55:02 +0000

    CORE-4956 adding context to the standalone app

--- src/main/java/com/boomi/flow/services/aws/rekognition/Application.java
+++ src/main/java/com/boomi/flow/services/aws/rekognition/Application.java
@@ -1,13 +1,24 @@
 package com.boomi.flow.services.aws.rekognition;
 
 import com.manywho.sdk.services.servers.EmbeddedServer;
+import com.manywho.sdk.services.servers.Servlet3Server;
 import com.manywho.sdk.services.servers.undertow.UndertowServer;
 
-public class Application {
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class Application extends Servlet3Server {
+
+    public Application() {
+        this.addModule(new ApplicationModule());
+        this.setApplication(Application.class);
+        this.start();
+    }
+
     public static void main(String[] args) throws Exception {
         EmbeddedServer server = new UndertowServer();
         server.addModule(new ApplicationModule());
         server.setApplication(Application.class);
-        server.start();
+        server.start("/api/aws/rekognition/1");
     }
 }
