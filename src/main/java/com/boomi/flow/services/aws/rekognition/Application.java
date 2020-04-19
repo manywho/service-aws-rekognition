a9708b0f9754db4637778f6c13337be72db7bcc9
Jonjo McKay <jonjo@jonjomckay.com>
2020-04-19 16:39:52 +0100

    FLOW-1564: Added stuff required for Kubernetes

--- src/main/java/com/boomi/flow/services/aws/rekognition/Application.java
+++ src/main/java/com/boomi/flow/services/aws/rekognition/Application.java
@@ -19,6 +19,6 @@ public class Application extends Servlet3Server {
         EmbeddedServer server = new UndertowServer();
         server.addModule(new ApplicationModule());
         server.setApplication(Application.class);
-        server.start("/api/aws/rekognition/1");
+        server.start();
     }
 }
